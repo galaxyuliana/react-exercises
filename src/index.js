82d724891de0876792f634ff59d2586968b9384e
galaxyuliana <yulianashin613@gmail.com>
2022-08-01 01:35:10 -0800

    Added RandomNumber

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Time from './Time';
+import RandomNumber from './RandomNumber';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <Time />
+    <RandomNumber />
   </React.StrictMode>
 );
 
