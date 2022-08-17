44f76d624bac1be9c547228b808639e4f3646962
galaxyuliana <yulianashin613@gmail.com>
2022-08-16 22:56:42 -0700

    Added RandomNumber

---
+++ src/RandomNumber.js
@@ -0,0 +1,11 @@
+function RandomNumber() {
+    return (
+      <div>
+        <h1>This is random rumber</h1>
+        <h2>It is {Math.random()}.</h2>
+      </div>
+    );
+  }
+  
+  export default RandomNumber;
+  
